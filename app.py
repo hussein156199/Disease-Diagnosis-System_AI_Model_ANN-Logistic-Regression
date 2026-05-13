b147adc1056b3299e7233d5be4008da89cbc539b
hussein <husseinessam180@gmail.com>
2026-05-13 06:48:52 +0300

    fix1

--- app.py
+++ app.py
@@ -580,5 +580,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    print("\n🌐 تشغيل التطبيق على http://localhost:5000")
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
